ededfa361337ba3ff88970d86ea3fbba041aff93
axchisan <leftyrancuentabot@gmail.com>
2025-05-29 11:39:35 -0500

    api key

--- app/chatbot/__init__.py
+++ app/chatbot/__init__.py
@@ -14,7 +14,7 @@ def init_chatbot():
     dynamic_datasets = load_dynamic_datasets("data/processed")
     
     # Configura la API de OpenAI 
-    api_key = "aca va la api"
+    api_key = "Api"
     
     # Inicializa el procesador de datasets con los datos dinámicos cargados.
     dataset_processor = DatasetProcessor()
